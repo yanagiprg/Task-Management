86eaba560a4ef8e5a290f0bc477607e7a63dde4b
yanagi_prg <takayanagi.prg@gmail.com>
2019-09-19 15:42:07 +0900

    write CRUD actions on controller & posts resources on routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,31 @@
 class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all.order(created_at: :asc)
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.create(title: params[:title], content: params[:content])
+    render json: @post
+
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post = Post.create(title: params[:title], content: params[:content])
+    render json: @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    render json: @post
+  end
+
 end
